e8094fb83f403cfe573c51b42032e7eb4f155a80
Tim Hickam <thickam@quesolutions.net>
2018-12-03 15:55:18 -0500

    Pulled some constants and animations out to own files

---
+++ src/constants.js
@@ -0,0 +1,94 @@
+export const SHOW_DEBUG = false;
+
+export const IGNORE_MAP_COLLISION = false;
+
+export const SLIME_START_COLOR = 'green';
+
+export const SLIME_SPEED = 100;
+
+export const TINT_MAP = {
+	red: 0xff0000,
+	green: 0x00ff00,
+	blue: 0x0000ff,
+	purple: 0xaa00ff,
+	yellow: 0xffff00
+};
+
+export const COMBAT_MAP = {
+	slime: {
+		red: {
+			current: 50,
+			max: 50,
+			attack: 25,
+			attackPeriod: 1000,
+			lastAttack: null
+		},
+		green: {
+			current: 50,
+			max: 50,
+			attack: 25,
+			attackPeriod: 1000,
+			lastAttack: null
+		},
+		blue: {
+			current: 50,
+			max: 50,
+			attack: 25,
+			attackPeriod: 1000,
+			lastAttack: null
+		},
+		purple: {
+			current: 50,
+			max: 50,
+			attack: 25,
+			attackPeriod: 1000,
+			lastAttack: null
+		},
+		yellow: {
+			current: 50,
+			max: 50,
+			attack: 25,
+			attackPeriod: 1000,
+			lastAttack: null
+		},
+	},
+	wizard: {
+		blue: {
+			current: 25,
+			max: 25,
+			attack: 10,
+			attackPeriod: 1000,
+			lastAttack: null
+		},
+		green: {
+			current: 55,
+			max: 55,
+			attack: 30,
+			attackPeriod: 1000,
+			lastAttack: null
+		},
+		red: {
+			current: 30,
+			max: 30,
+			attack: 10,
+			attackPeriod: 1000,
+			lastAttack: null
+		}
+	},
+	knight: {
+		red: {
+			current: 55,
+			max: 55,
+			attack: 10,
+			attackPeriod: 1000,
+			lastAttack: null
+		},
+		silver: {
+			current: 25,
+			max: 25,
+			attack: 10,
+			attackPeriod: 1000,
+			lastAttack: null
+		}
+	}
+};
